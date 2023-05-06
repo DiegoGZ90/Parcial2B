04892ddb74338f2b220cea95daa68140e5d04664
Diego <mrtomacoyt@gmail.com>
2023-05-06 08:12:11 -0600

    primer comit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+
+string linea;
+string texto;
+int main (){
+    ifstream archivo("documento.txt");
+    while (getline(archivo, linea)){
+        texto = texto + linea + "\n";
+
+    }
+
+
+     archivo.close();
+     cout<< texto <<endl;
+
+    ofstream archivo2("documento.txt");
+    archivo2<< texto << "nueva linea de texto ";
+        system("pause");
+
+        cout<<"Hola";
+
+
+}
+
